1053d88a5d01d77c2ea5bc4958e37379d55cfdf9
Cristian Vidmar <cristian.vidmar@icloud.com>
2021-03-30 11:30:36 +0200

    getAllAssets fix privatize ContentfulClient fields

--- erm/templates/templates.go
+++ erm/templates/templates.go
@@ -2,14 +2,14 @@ package templates
 
 import _ "embed"
 
-//go:embed "contentful_vo.tmpl"
+//go:embed "contentful_vo.gotmpl"
 var TemplateVo []byte
 
-//go:embed "contentful_vo_base.tmpl"
+//go:embed "contentful_vo_base.gotmpl"
 var TemplateVoBase []byte
 
-//go:embed "contentful_vo_lib.tmpl"
+//go:embed "contentful_vo_lib.gotmpl"
 var TemplateVoLib []byte
 
-//go:embed "contentful_vo_lib_contenttype.tmpl"
+//go:embed "contentful_vo_lib_contenttype.gotmpl"
 var TemplateVoLibContentType []byte
