243365a59347364f9c7c68de1beee854c56e4351
Cristian Vidmar <cristian.vidmar@icloud.com>
2022-09-30 14:26:14 +0200

    chore: version bump

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 	"github.com/foomo/gocontentful/erm"
 )
 
-var VERSION = "v1.0.12"
+var VERSION = "v1.0.13"
 
 type contentfulRc struct {
 	ManagementToken string `json:"managementToken"`
