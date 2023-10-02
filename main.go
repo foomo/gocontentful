9b9fdad2cddfdf82d9555d62305900ceaafc94b8
Cristian Vidmar <cristian.vidmar@icloud.com>
2023-10-02 17:50:10 +0200

    fix: race condition on sync mode

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 	"github.com/foomo/gocontentful/erm"
 )
 
-var VERSION = "v1.0.23"
+var VERSION = "v1.0.25"
 
 type contentfulRc struct {
 	ManagementToken string `json:"managementToken"`
