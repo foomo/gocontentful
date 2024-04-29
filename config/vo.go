5738aee97c7b47b6496c18f1310cb78b83c94d51
Cristian Vidmar <cristian.vidmar@icloud.com>
2024-04-29 15:41:52 +0200

    chore: linting

--- config/vo.go
+++ config/vo.go
@@ -1,10 +1,10 @@
 package config
 
 type Config struct {
-	SpaceID           string   `yaml:"spaceId,omitempty"`
-	Environment       string   `yaml:"environment,omitempty"`
-	ExportFile        string   `yaml:"exportFile,omitempty"`
-	ContentTypes      []string `yaml:"contentTypes,omitempty"`
-	PathTargetPackage string   `yaml:"pathTargetPackage,omitempty"`
-	RequireVersion    string   `yaml:"requireVersion,omitempty"`
+	SpaceID           string   `json:"spaceId,omitempty" yaml:"spaceId,omitempty"`
+	Environment       string   `json:"environment,omitempty" yaml:"environment,omitempty"`
+	ExportFile        string   `json:"exportFile,omitempty" yaml:"exportFile,omitempty"`
+	ContentTypes      []string `json:"contentTypes,omitempty" yaml:"contentTypes,omitempty"`
+	PathTargetPackage string   `json:"pathTargetPackage,omitempty" yaml:"pathTargetPackage,omitempty"`
+	RequireVersion    string   `json:"requireVersion,omitempty" yaml:"requireVersion,omitempty"`
 }
